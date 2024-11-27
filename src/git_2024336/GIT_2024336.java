403126117ff4e1c9c0b0213d912a48fa939195df
VitorOliveiraTrindadeCCT <2024336@student.cct.ie>
2024-11-27 15:08:30 +0000

    Add Line

--- src/git_2024336/GIT_2024336.java
+++ src/git_2024336/GIT_2024336.java
@@ -15,6 +15,7 @@ public class GIT_2024336 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Vitor Oliveira Trindade");
     }
     
 }
